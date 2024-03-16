6135526e14eb9640a98ac62b9ab91b447375bdcd
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-03-16 22:56:04 +0700

    create design for cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,13 +1,27 @@
-import { Component } from "@angular/core";
-import { Cart } from "../../models/cart.model";
+import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "../../models/cart.model";
+import { RouterModule } from "@angular/router";
+import { CurrencyPipe } from "@angular/common";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: "app-cart",
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CurrencyPipe,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+  ],
   templateUrl: "./cart.component.html",
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
   cart: Cart = {
     items: [
       {
@@ -17,6 +31,40 @@ export class CartComponent {
         price: 150,
         quantity: 1,
       },
+      {
+        id: 1,
+        product: "https://via.placeholder.com/150",
+        name: "snickers",
+        price: 150,
+        quantity: 2,
+      },
+      {
+        id: 1,
+        product: "https://via.placeholder.com/150",
+        name: "snickers",
+        price: 150,
+        quantity: 1,
+      },
     ],
   };
+
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  public getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((acc, totalPricePerItem) => acc + totalPricePerItem, 0);
+  }
 }
