fef34ced83e71007f593621c26aad6c4eb4867a0
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-03-16 20:38:43 +0700

    add pages/components: home - filters

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,25 @@
 import { Component } from "@angular/core";
 
 import { ProductsHeaderComponent } from "./components/products-header/products-header.component";
+import { FiltersComponent } from "./components/filters/filters.component";
 
 import { MatSidenavModule } from "@angular/material/sidenav";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [MatSidenavModule, ProductsHeaderComponent],
+  imports: [ProductsHeaderComponent, FiltersComponent, MatSidenavModule],
   templateUrl: "./home.component.html",
 })
 export class HomeComponent {
+  category: string | undefined;
   cols = 3;
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
