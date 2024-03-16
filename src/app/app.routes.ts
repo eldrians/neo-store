89ab51c9f47065447557e6711c61ebd2c13d0097
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-03-16 12:42:52 +0700

    add route: /home and default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
-import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
   {
-    path: "header",
-    component: HeaderComponent,
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
 ];
